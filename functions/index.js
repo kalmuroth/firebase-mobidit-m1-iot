d0d1b907e335f8860ae8eb2d2d03cba6ea72b242
kalmuroth <dokkan.dracktar@gmail.com>
2023-06-27 14:28:47 +0200

    added upload to bucket method

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const express = require("express");
+const formidable = require('formidable-serverless');
+const gcs = require('@google-cloud/storage');
+
 
 admin.initializeApp();
 
@@ -230,6 +233,9 @@ app_user.delete("/:id", async (req, res) => {
     res.status(200).send();
 })
 
+exports.user = functions.region('europe-west2').https.onRequest(app_user);
+
+//on user.create in Firebase Auth => method trigger => add user in database (uuid/email)
 exports.updateUserCount = functions.region('europe-west2').auth.user().onCreate(async (user) => {
     try {
       const { uid, email} = user;
@@ -243,4 +249,34 @@ exports.updateUserCount = functions.region('europe-west2').auth.user().onCreate(
     }
 });
 
-exports.user = functions.region('europe-west2').https.onRequest(app_user);
+
+//upload image to firebase bucket
+exports.uploadFile = functions.region('europe-west2').https.onRequest((req, res) => {
+    var form = new formidable.IncomingForm();
+    return new Promise((resolve, reject) => {
+      form.parse(req, function(err, fields, files) {
+        var file = files.fileToUpload;
+        if(!file){
+          reject("no file to upload, please choose a file.");
+          return;
+        }
+        var filePath = file.path;  
+        var bucket = admin.storage().bucket('flutter-mobidit-m1-iot.appspot.com');
+        return bucket.upload(filePath, {
+          destination: file.name
+        }).then(() => {
+          // Get the full URL of the uploaded file
+          const fileUrl = `https://storage.googleapis.com/${bucket.name}/${file.name}`;
+          resolve(fileUrl);
+        }).catch((err) => {
+          reject('Failed to upload: ' + JSON.stringify(err));
+        });
+      });
+    }).then((fileUrl) => {
+      res.status(200).send(JSON.stringify(fileUrl));
+      return null
+    }).catch(err => {
+      console.error('Error while parsing form: ' + err);
+      res.status(500).send('Error while parsing form: ' + err);
+    });
+});
