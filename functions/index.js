0163e2c2057663a25ec7cafbefa92676940c2498
kalmuroth <dokkan.dracktar@gmail.com>
2023-06-29 09:56:14 +0200

    uploadFileInBase64 is working

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,13 @@ const functions = require("firebase-functions");
 const express = require("express");
 const formidable = require('formidable-serverless');
 const gcs = require('@google-cloud/storage');
-
+const fetch = require('node-fetch');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+require('dotenv').config();
 
 admin.initializeApp();
 
@@ -16,6 +22,7 @@ en ligne de commande : $ firebase emulators:start
 dans postman mettre http://localhost:5001/bluelocker1-2/us-central1/<function_name>
 
 */
+
 
 //--------------------------------------------------------------
 //--------ROUTE TO ADMINISTRATE THE CATEGORY--------------------
@@ -197,7 +204,8 @@ app_user.get("/:id", async (req, res) => {
 app_user.get("/", async (req, res) => {
     const snapshot = await admin.firestore().collection("users").get();
     const keys = [];
-    
+    const speudo = getRandomUsername()
+    console.log(speudo)
     snapshot.forEach((doc) => {
         const keyId = doc.id;
         const keyData = doc.data();
@@ -237,46 +245,109 @@ exports.user = functions.region('europe-west2').https.onRequest(app_user);
 
 //on user.create in Firebase Auth => method trigger => add user in database (uuid/email)
 exports.updateUserCount = functions.region('europe-west2').auth.user().onCreate(async (user) => {
-    try {
-      const { uid, email} = user;
-      const docRef = admin.firestore().collection("users").doc(uid);
-      await docRef.set({ uid, email});
-  
-      return;
-    } catch (error) {
-      console.error("Error updating user count:", error);
-      throw error;
-    }
+  try {
+    const { uid, email} = user;
+    const docRef = admin.firestore().collection("users").doc(uid);
+    await docRef.set({ uid, email});
+
+    return;
+  } catch (error) {
+    console.error("Error updating user count:", error);
+    throw error;
+  }
 });
 
 
 //upload image to firebase bucket
 exports.uploadFile = functions.region('europe-west2').https.onRequest((req, res) => {
-    var form = new formidable.IncomingForm();
-    return new Promise((resolve, reject) => {
-      form.parse(req, function(err, fields, files) {
-        var file = files.fileToUpload;
-        if(!file){
-          reject("no file to upload, please choose a file.");
-          return;
-        }
-        var filePath = file.path;  
-        var bucket = admin.storage().bucket('flutter-mobidit-m1-iot.appspot.com');
-        return bucket.upload(filePath, {
-          destination: file.name
-        }).then(() => {
-          // Get the full URL of the uploaded file
-          const fileUrl = `https://storage.googleapis.com/${bucket.name}/${file.name}`;
-          resolve(fileUrl);
-        }).catch((err) => {
-          reject('Failed to upload: ' + JSON.stringify(err));
-        });
+  var form = new formidable.IncomingForm();
+  return new Promise((resolve, reject) => {
+    form.parse(req, function(err, fields, files) {
+      var file = files.fileToUpload;
+      if(!file){
+        reject("no file to upload, please choose a file.");
+        return;
+      }
+      var filePath = file.path;  
+      var bucket = admin.storage().bucket('flutter-mobidit-m1-iot.appspot.com');
+      return bucket.upload(filePath, {
+        destination: file.name
+      }).then(() => {
+        // Get the full URL of the uploaded file
+        const fileUrl = `https://storage.googleapis.com/${bucket.name}/${file.name}`;
+        resolve(fileUrl);
+      }).catch((err) => {
+        reject('Failed to upload: ' + JSON.stringify(err));
       });
-    }).then((fileUrl) => {
-      res.status(200).send(JSON.stringify(fileUrl));
-      return null
-    }).catch(err => {
-      console.error('Error while parsing form: ' + err);
-      res.status(500).send('Error while parsing form: ' + err);
     });
+  }).then((fileUrl) => {
+    res.status(200).send(JSON.stringify(fileUrl));
+    return null
+  }).catch(err => {
+    console.error('Error while parsing form: ' + err);
+    res.status(500).send('Error while parsing form: ' + err);
+  });
 });
+
+
+//same but decode in base64 
+exports.uploadFileInBase64 = functions.region('europe-west2').https.onRequest(async (req, res) => {
+  const fileData = req.body.fileData;
+
+  if (!fileData) {
+    res.status(400).send('No file data provided. Please choose a file.');
+    return;
+  }
+
+  try {
+    const fileBuffer = Buffer.from(fileData, 'base64');
+    const tempFilePath = path.join(os.tmpdir(), 'temp-image.jpg');
+
+    fs.writeFileSync(tempFilePath, fileBuffer);
+
+    const storageWithAuth = new Storage({
+      projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+      credentials: {
+        client_email: process.env.CLIENT_EMAIL,
+        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace newline characters
+      },
+    });
+
+    const bucket = storage.bucket('flutter-mobidit-m1-iot.appspot.com');
+    const bucketName ='flutter-mobidit-m1-iot.appspot.com';
+    const fileName = 'image.jpg';
+
+    const [uploadedFile] = await storageWithAuth.bucket(bucketName).upload(tempFilePath, {
+      destination: fileName,
+      contentType: 'image/jpg',
+    });
+
+    const file = bucket.file(fileName);
+    const [metadata] = await file.getMetadata();
+    const downloadUrl = await uploadedFile.getSignedUrl({
+      action: 'read',
+      expires: '03-01-2500', // Set an appropriate expiration date
+    });
+
+    res.status(200).send(JSON.stringify(downloadUrl[0]));
+  } catch (err) {
+    console.error('Error while decoding or uploading the file: ' + err);
+    res.status(500).send('Error while decoding or uploading the file: ' + err);
+  }
+});
+
+
+
+//generate random username
+async function getRandomUsername() {
+  try {
+    const response = await fetch('https://randomuser.me/api/');
+    const data = await response.json();
+    const username = data.results[0].login.username;
+    return username;
+  } catch (error) {
+    console.log('Error:', error);
+    // Handle error
+    return null;
+  }
+}
